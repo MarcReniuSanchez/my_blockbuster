32373a1d693d479cc3821b9495fdd1e55d8240e9
MarcReniuSanchez <marc.reniu.sanchez@gmail.com>
2020-03-29 14:05:33 +0200

    Added comparison test for Media API REST response

--- app/models/media.rb
+++ app/models/media.rb
@@ -4,4 +4,6 @@ class Media < ApplicationRecord
   has_many :purchase_options, dependent: :destroy
 
   validates :title, :plot, presence: true
+
+  scope :by_creation, -> { order(created_at: :asc) }
 end
--- spec/requests/medias_request_spec.rb
+++ spec/requests/medias_request_spec.rb
@@ -12,6 +12,7 @@ RSpec.describe "Medias API", type: :request do
     it "returns medias" do
       expect(json_response).not_to be_empty
       expect(json_response.size).to eq(8)
+      expect(json_response).to match(media_expected_response)
     end
   end
 
@@ -21,6 +22,7 @@ RSpec.describe "Medias API", type: :request do
     it "returns movies" do
       expect(json_response).not_to be_empty
       expect(json_response.size).to eq(5)
+      expect(json_response).to match(movie_expected_response)
     end
   end
 
@@ -30,6 +32,26 @@ RSpec.describe "Medias API", type: :request do
     it "returns season" do
       expect(json_response).not_to be_empty
       expect(json_response.size).to eq(3)
+      expect(json_response).to match(season_expected_response)
     end
+  end
+end
+
+def media_expected_response
+  Media.by_creation.collect do |media|
+    # In order to not deal with the STI class
+    (media.class.name + "Serializer").constantize.new(media).as_json.with_indifferent_access
+  end
+end
+
+def movie_expected_response
+  Movie.by_creation.collect do |movie|
+    MovieSerializer.new(movie).as_json.with_indifferent_access
+  end
+end
+
+def season_expected_response
+  Season.by_creation.collect do |season|
+    SeasonSerializer.new(season).as_json.with_indifferent_access
   end
 end
